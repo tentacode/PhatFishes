e9307060ef2dab75fdb6a4d0619f29368ac83e65
tentacode <contact@gabrielpillet.com>
2016-04-16 20:51:16 +0200

    Multiplayer inputs

--- Assets/_Scripts/Fish/FishInput.cs
+++ Assets/_Scripts/Fish/FishInput.cs
@@ -4,21 +4,24 @@ public class FishInput : MonoBehaviour
 {
     private FishPhysics fishPhysics;
     private FishMover fishMover;
+    private FishPlayer fishPlayer;
 
     void Start()
     {
         fishPhysics = GetComponent<FishPhysics>();
         fishMover = GetComponent<FishMover>();
+        fishPlayer = GetComponent<FishPlayer>();
     }
 
     void FixedUpdate()
     {
-        if (Input.GetButtonDown("P1Action")) {
+        var label = "P" + fishPlayer.playerIndex;
+        if (Input.GetButtonDown(label + "Action")) {
             fishPhysics.ToggleBlow();
         }
 
-        float horizontalValue = Input.GetAxis("P1Horizontal");
-        float verticalValue = Input.GetAxis("P1Vertical");
+        float horizontalValue = Input.GetAxis(label + "Horizontal");
+        float verticalValue = Input.GetAxis(label + "Vertical");
 
         fishMover.Move(horizontalValue, verticalValue);
     }
---
+++ Assets/_Scripts/Fish/FishPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishPlayer : MonoBehaviour
+{
+    public int playerIndex;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
