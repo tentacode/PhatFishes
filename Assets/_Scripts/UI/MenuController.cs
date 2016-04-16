6bc158bf42c2798b6e16f8b4e880ebfe01203fed
tentacode <contact@gabrielpillet.com>
2016-04-16 23:58:54 +0200

    UI

--- Assets/_Scripts/UI/MenuController.cs
+++ Assets/_Scripts/UI/MenuController.cs
@@ -4,34 +4,56 @@
 
 public class MenuController : MonoBehaviour
 {
-    public Canvas mainMenu;
-    public Canvas optionMenu;
+    public GameObject mainMenu;
+    public GameObject optionMenu;
 
     public InputField player1Name;
     public InputField player2Name;
     public InputField player3Name;
     public InputField player4Name;
 
+    private bool isSetup = true;
+
     void Start()
     {
-        player1Name.text = PlayerPrefs.GetString("Player1Name");
-        player2Name.text = PlayerPrefs.GetString("Player2Name");
-        player3Name.text = PlayerPrefs.GetString("Player3Name");
-        player4Name.text = PlayerPrefs.GetString("Player4Name");
+        if (PlayerPrefs.HasKey("Player1Name")) {
+            player1Name.text = PlayerPrefs.GetString("Player1Name");
+        }
+
+        if (PlayerPrefs.HasKey("Player2Name")) {
+            player2Name.text = PlayerPrefs.GetString("Player2Name");
+        }
+
+        if (PlayerPrefs.HasKey("Player3Name")) {
+            player3Name.text = PlayerPrefs.GetString("Player3Name");
+        }
+
+        if (PlayerPrefs.HasKey("Player4Name")) {
+            player4Name.text = PlayerPrefs.GetString("Player4Name");
+        }
+
+        isSetup = false;
 
         SwitchToMainMenu();
     }
 
+    void Update()
+    {
+        if (mainMenu.activeSelf == false && Input.GetButtonDown("MenuCancel")){
+            SwitchToMainMenu();
+        }
+    }
+
     public void SwitchToMainMenu()
     {
-        optionMenu.enabled = false;
-        mainMenu.enabled = true;
+        optionMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 
     public void SwitchToOptions()
     {
-        mainMenu.enabled = false;
-        optionMenu.enabled = true;
+        mainMenu.SetActive(false);
+        optionMenu.SetActive(true);
     }
 
     public void SwitchToGame()
@@ -46,9 +68,14 @@ public class MenuController : MonoBehaviour
 
     public void SetPlayerNames()
     {
+        if (isSetup) {
+            return;
+        }
+
         PlayerPrefs.SetString("Player1Name", player1Name.text);
         PlayerPrefs.SetString("Player2Name", player2Name.text);
         PlayerPrefs.SetString("Player3Name", player3Name.text);
         PlayerPrefs.SetString("Player4Name", player4Name.text);
+        PlayerPrefs.Save();
     }
 }
