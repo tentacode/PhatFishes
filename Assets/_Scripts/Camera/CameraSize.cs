84355d18bdd2a669f85c077a849362480c106739
tentacode <contact@gabrielpillet.com>
2016-04-16 11:00:50 +0200

    Fish bounce

---
+++ Assets/_Scripts/Camera/CameraSize.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSize : MonoBehaviour
+{
+    public int pixelScaling = 6;
+
+    private int PIXEL_PER_UNIT = 10;
+
+    void Start()
+    {
+        Camera camera = GetComponent<Camera>();
+        camera.orthographicSize = Screen.height * 0.5f / (pixelScaling * PIXEL_PER_UNIT);
+    }
+}
---
+++ Assets/_Scripts/Fish/FishMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishMover : MonoBehaviour
+{
+    public float horizontalSpeed = 1.0f;
+    public float verticalSpeed = 1.0f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalValue = Input.GetAxis("P1Horizontal");
+        if (horizontalValue != 0) {
+            rb.AddForce(Vector2.right * horizontalValue * horizontalSpeed);
+        }
+
+        float verticalValue = Input.GetAxis("P1Vertical");
+        if (verticalValue != 0) {
+            rb.AddForce(Vector2.up * verticalValue * verticalSpeed);
+        }
+	}
+}
