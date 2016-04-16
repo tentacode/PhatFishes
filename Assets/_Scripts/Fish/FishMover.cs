023c570269b3fa93bdbe942fd3f7d16462ed7dfe
tentacode <contact@gabrielpillet.com>
2016-04-16 13:49:08 +0200

    Fish animations

--- Assets/_Scripts/Fish/FishMover.cs
+++ Assets/_Scripts/Fish/FishMover.cs
@@ -7,10 +7,29 @@ public class FishMover : MonoBehaviour
     public float verticalSpeed = 1.0f;
 
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private bool isShrink = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("P1Action")) {
+            if (isShrink) {
+                animator.SetTrigger("Blow");
+            }
+            else {
+                animator.SetTrigger("Shrink");
+            }
+
+            isShrink = !isShrink;
+        }
     }
 
     void FixedUpdate()
@@ -18,6 +37,8 @@ public class FishMover : MonoBehaviour
         float horizontalValue = Input.GetAxis("P1Horizontal");
         if (horizontalValue != 0) {
             rb.AddForce(Vector2.right * horizontalValue * horizontalSpeed);
+
+            spriteRenderer.flipX = horizontalValue > 0;
         }
 
         float verticalValue = Input.GetAxis("P1Vertical");
