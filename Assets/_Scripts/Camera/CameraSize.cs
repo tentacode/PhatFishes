9e8447c4777433a377440e29ba040099cae84bb0
tentacode <contact@gabrielpillet.com>
2016-04-17 13:52:13 +0200

    Particles

--- Assets/_Scripts/Camera/CameraSize.cs
+++ Assets/_Scripts/Camera/CameraSize.cs
@@ -9,6 +9,12 @@ public class CameraSize : MonoBehaviour
 
     void Start()
     {
+        pixelScaling = Mathf.FloorToInt(Screen.height / 256);
+
+        if (Mathf.FloorToInt(Screen.width / 512) < pixelScaling) {
+            pixelScaling = Mathf.FloorToInt(Screen.width / 512);
+        }
+
         Camera camera = GetComponent<Camera>();
         camera.orthographicSize = Screen.height * 0.5f / (pixelScaling * PIXEL_PER_UNIT);
     }
